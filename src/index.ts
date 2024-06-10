710d9b4b3e2e67515dda6e15fa40bf1913bc154d
branberry <brandon.ly@mongodb.com>
2024-06-10 11:34:40 -0500

    Add logic to save cache files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,37 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyIntegration } from "@netlify/sdk";
-import { execFile } from "child_process";
+import { readdir } from "fs";
 
 import { promisify } from "util";
 
-const execFileAsync = promisify(execFile);
+const readdirAsync = promisify(readdir);
+
+async function getCacheFilePaths(): Promise<string[]> {
+  const filesPaths = await readdirAsync(process.cwd());
+
+  const cacheFilePaths = filesPaths.filter((filePath) =>
+    filePath.endsWith(".cache.gz")
+  );
+  return cacheFilePaths;
+}
 const integration = new NetlifyIntegration();
 
-integration.addBuildEventHandler("onSuccess", async ({ utils: { cache } }) => {
-  console.log("Creating cache files...");
-  await execFileAsync("./snooty-parser/snooty/snooty", ["create-cache", "."]);
-  console.log("Cache files created");
-});
+integration.addBuildEventHandler(
+  "onSuccess",
+  async ({ utils: { run, cache } }) => {
+    console.log("Creating cache files...");
+    await run.command("./snooty-parser/snooty/snooty create-cache .");
+    console.log("Cache files created");
+
+    const cacheFiles = await getCacheFilePaths();
+
+    await Promise.all(
+      cacheFiles.map(async (filePath) => {
+        console.log(`Adding cache file: ${filePath}`);
+        await cache.save(filePath);
+      })
+    );
+  }
+);
 
 export { integration };
