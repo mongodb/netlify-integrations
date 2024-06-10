53f03a3a2a86aa28495e64810564c13b176e47e1
branberry <brandon.ly@mongodb.com>
2024-06-10 11:13:36 -0500

    update integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyIntegration } from "@netlify/sdk";
+import { execFile } from "child_process";
 
+import { promisify } from "util";
+
+const execFileAsync = promisify(execFile);
 const integration = new NetlifyIntegration();
 
-integration.addBuildEventHandler("onPreBuild", ({ utils: { cache } }) => {
-  console.log("Hello there.");
+integration.addBuildEventHandler("onSuccess", async ({ utils: { cache } }) => {
+  console.log("Creating cache files...");
+  await execFileAsync("./snooty-parser/snooty/snooty", ["create-cache", "."]);
+  console.log("Cache files created");
 });
-  
-export { integration };
 
+export { integration };
