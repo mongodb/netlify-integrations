1f8d19822eb7916b4f6905098e86a67173123b03
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-06-25 18:04:34 -0400

    read cwd

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyIntegration } from "@netlify/sdk";
+import { readdir } from "fs";
+
+import { promisify } from "util";
+
+const readdirAsync = promisify(readdir);
 
 const integration = new NetlifyIntegration();
 
-integration.addBuildEventHandler("onSuccess", () => {
+integration.addBuildEventHandler("onSuccess", async () => {
+  const filesPaths = await readdirAsync(process.cwd());
   console.log("Hello there.");
+  console.log(filesPaths);
 });
 
 export { integration };
