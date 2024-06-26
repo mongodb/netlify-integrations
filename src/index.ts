ef79bd127dad119e34b66a54373d5807ce793658
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-06-26 13:56:26 -0400

    add manifest interfaces, beginnings of generate_manifest

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,39 @@ const readdirAsync = promisify(readdir);
 
 const integration = new NetlifyIntegration();
 
+interface Manifest {
+  url?: URL;
+  includeInGlobalSearch: boolean;
+  documents: ManifestEntry[];
+}
+
+interface ManifestEntry {
+  slug: string;
+  title?: string[];
+  headings?: string[][];
+  paragraphs: string;
+  code: {};
+  preview?: string;
+  tags: string[];
+  facets: any;
+}
+
+const generate_manifest = (filePath: any) => {
+  console.log("generating manifest function");
+  const manifest: Manifest = {
+    includeInGlobalSearch: true,
+    documents: [] as ManifestEntry[],
+  };
+  return manifest;
+};
 integration.addBuildEventHandler("onSuccess", async () => {
-  const filesPaths = (await readdirAsync(process.cwd())).filter((filePath) =>
+  const filePath = (await readdirAsync(process.cwd())).filter((filePath) =>
     filePath.match("bundle.zip")
   );
+
   console.log("Hello, logging bundle.zip.");
-  console.log(filesPaths);
+  console.log(filePath);
+  const manifest = generate_manifest(filePath);
 });
 
 export { integration };
