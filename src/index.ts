8d95fa031fac3511f4c0f52f3aa3d806fb17e401
branberry <brandon.ly@mongodb.com>
2024-07-02 15:06:37 -0500

    Add recursive check

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyIntegration } from "@netlify/sdk";
+import { readdir } from "fs";
+import { promisify } from "util";
 
+const readdirAsync = promisify(readdir);
 const integration = new NetlifyIntegration();
 
 integration.addBuildEventHandler("onSuccess", async ({ utils: { run } }) => {
   console.log("=========== Chatbot Data Upload Integration ================");
   await run.command("unzip bundle.zip -d bundle");
+  const zipPages = readdirAsync(`${process.cwd()}/bundle/documents`, {
+    recursive: true,
+  });
+
+  console.log("ZipPages: ", zipPages);
 
   console.log("=========== Chatbot Data Upload Integration ================");
 });
