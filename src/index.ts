cd4f497221dd924d939f3ba9c1256ef5bd463afd
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-07-02 17:38:40 -0400

    undoing document class

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ interface ManifestEntry {
 const processManifest = (decodedFile: any) => {
   //put file into Document object
   //export Document object
-  const doc = new Document(decodedFile);
+  // const doc = new Document(decodedFile);
   return decodedFile;
 };
 
