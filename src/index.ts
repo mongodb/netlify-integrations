7e9d51492e30b68846a1e641d95ede0cb14e5064
branberry <brandon.ly@mongodb.com>
2024-07-02 14:35:43 -0500

    Wrangle types (spent way too long on this)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyIntegration } from "@netlify/sdk";
+import type { ExecaChildPromise, ExecaReturnValue } from "execa";
+import { ChildProcess } from "child_process";
 
+// Added this type to get SOME intellisense
+type ExecaChildProcess<StdoutErrorType = string> = ChildProcess &
+  ExecaChildPromise<StdoutErrorType> &
+  Promise<ExecaReturnValue<StdoutErrorType>>;
 const integration = new NetlifyIntegration();
 
-integration.addBuildEventHandler("onPreBuild", () => {
-  console.log("Hello there.");
-});
-  
-export { integration };
+integration.addBuildEventHandler("onPostBuild", async ({ utils: { run } }) => {
+  const result: ExecaChildProcess = await run.command(
+    "unzip bundle.zip -d bundle"
+  );
 
+  console.log("stdin: ", result.stdin);
+});
+
+export { integration };
