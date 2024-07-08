8f59375c51992efbf2ea811a82767f47b2b0a5d0
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-07-08 15:29:22 -0400

    logging

--- src/document.ts
+++ src/document.ts
@@ -22,7 +22,7 @@ export class Document {
     console.log("called doc");
     //find metadata
     [this.robots, this.keywords, this.description] = this.findMetadata();
-
+    this.paragraphs = this.findParagraphs();
     //find paragraphs
     //find code
     //find title, headings
@@ -42,21 +42,27 @@ export class Document {
       path: "$..children[?(@.type=='heading')].children",
       json: this.tree,
     });
-    if (results) {
-      const val = results[0].value;
-      //check if robots, set to false if no robots
-      if ("robots" in val && (val.robots == "None" || val.robots == "noindex"))
-        robots = false;
+    console.log("results:", results);
+    console.log("value:", results[0]);
+    // if (results) {
+    //   const val = results[0].value;
+    //   //check if robots, set to false if no robots
+    //   if ("robots" in val && (val.robots == "None" || val.robots == "noindex"))
+    //     robots = false;
 
-      if (val.includes("keywords")) {
-        keywords = val.keywords;
-      }
-      if (val.includes("description")) {
-        keywords = val.description;
-      }
-      return [robots, keywords, description];
-    }
+    //   if (val.includes("keywords")) {
+    //     keywords = val.keywords;
+    //   }
+    //   if (val.includes("description")) {
+    //     keywords = val.description;
+    //   }
+    //   return [robots, keywords, description];
+    // }
 
     return [];
+  }
+
+  findParagraphs() {
+    console.log("Finding paragraphs");
   }
 }
