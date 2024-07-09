7d03e36f64a2c60602810776c0a3fc446c8cca9e
branberry <brandon.ly@mongodb.com>
2024-07-09 08:47:39 -0500

    Add redoc install

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,30 @@
 import { NetlifyIntegration } from "@netlify/sdk";
 
 const integration = new NetlifyIntegration();
+const REDOC_CLI_VERSION = "1.2.3";
 
-integration.addBuildEventHandler("onPreBuild", () => {
-  console.log("Hello there.");
-});
-  
+// handle installing redoc cli if it's not already installed
+integration.addBuildEventHandler(
+  "onPreBuild",
+  async ({ utils: { run, cache } }) => {
+    const hasRedoc = await cache.has("redoc");
+    if (hasRedoc) return;
+
+    run.command(`
+    git clone -b @dop/redoc-cli@${REDOC_CLI_VERSION} --depth 1 https://github.com/mongodb-forks/redoc.git redoc \
+    # Install dependencies for Redoc CLI
+    && cd redoc/ \
+    && npm ci --prefix cli/ --omit=dev
+  `);
+
+    cache.save("redoc");
+  }
+);
+
+// handle building the redoc pages
+integration.addBuildEventHandler(
+  "onPostBuild",
+  async ({ utils: { run } }) => {}
+);
+
 export { integration };
-
