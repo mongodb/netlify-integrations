2bb5752674a61e0206d67a1557de87ecc40c0af8
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-07-15 15:00:09 -0400

    comment out vitest, modify config

--- tests/unit/facets.test.ts
+++ tests/unit/facets.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from "vitest";
+// import { expect, test } from "vitest";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(3).toBe(3);
-  console.log("testing");
-});
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(3).toBe(3);
+//   console.log("testing");
+// });
