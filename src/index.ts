207d7fb371fdc1d733248b15784a937d58cfa7dc
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-07-30 15:34:11 -0400

    filtering

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,9 @@ export const generateManifest = async (path?: any) => {
   const manifest = new Manifest(true);
   console.log("in generate manifest");
   //go into documents directory and get list of file entries
-  const entries = await readdirAsync(`${process.cwd()}/documents`, {
+  const entries = await readdirAsync(`/documents`, {
     recursive: true,
   });
-  const pro = await readdirAsync(`${process.cwd()}/documents`);
-  console.log(pro);
-  console.log("entries:" + JSON.stringify(entries));
 
   const mappedEntries = entries.filter((fileName) => {
     return (
