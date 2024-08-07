937d33a46a4819a0c890ad29212eaecc179c1f46
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-08-07 00:51:47 -0400

    loggin

--- src/connector.ts
+++ src/connector.ts
@@ -3,12 +3,11 @@
 // When adding helpers here, ask yourself if the helper will be used by more than one service
 // If no, the helper should be implemented in that service, not here
 
-import * as mongodb from "mongodb";
-import { Db } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 
 // We should only ever have one client active at a time.
 const atlasURL = `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_HOST}/?retryWrites=true&w=majority&maxPoolSize=20`;
-const client = new mongodb.MongoClient(atlasURL);
+const client = await MongoClient.connect(atlasURL);
 
 export const teardown = async () => {
   await client.close();
@@ -23,8 +22,9 @@ export const db = async () => {
   console.log("initiating db", process.env.MONGO_ATLAS_PASSWORD);
   if (!dbInstance) {
     try {
-      const result = client.connect();
-      console.log("connected to db", result);
+      // const client = await MongoClient.connect(atlasUri);
+      // const result = await client.connect();
+      console.log("connected to db", client);
       dbInstance = client.db(SNOOTY_DB_NAME);
       console.log("CONNECTED TO DB", dbInstance);
     } catch (error) {
