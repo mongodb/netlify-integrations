c506eff6e4b2e2a2714103c8d3ecf21939f5909c
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-08-08 19:29:25 -0400

    log environment type

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ integration.addBuildEventHandler(
     // repoName = repoName.split("/").pop();
     // console.log("repoName:", repoName);
     await run.command("unzip -o bundle.zip");
-    console.log("ENV type:", typeof netlifyConfig.build?.environment);
+    console.log("ENV keys:", netlifyConfig.build?.environment.keys());
     console.log("BRANCH:", netlifyConfig.build?.environment.branch);
     const branch = netlifyConfig.build?.environment;
 
