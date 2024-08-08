8abbc9094a5b6d116e9bcb995de4067ebae366b7
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-08-08 16:18:52 -0400

    pass in repoName, branch

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ integration.addBuildEventHandler(
     repoName = repoName.split("/").pop();
     console.log("repoName:", repoName);
     await run.command("unzip -o bundle.zip");
-    console.log("CONTEXT:", netlifyConfig.build?.environment);
+    console.log("CONTEXT:", JSON.stringify(netlifyConfig.build?.environment));
     const branch = netlifyConfig.build?.environment;
 
     //this export function is likely not needed
