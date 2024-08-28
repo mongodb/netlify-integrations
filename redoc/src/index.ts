0f7fe5ad1eb08e376410ab56d97cfb17cbb452cb
branberry <brandon.ly@mongodb.com>
2024-08-28 10:06:40 -0500

    Rename directories to have less redundant info

--- redoc/src/index.ts
+++ redoc/src/index.ts
@@ -20,7 +20,7 @@ export type OASPagesMetadata = Record<string, OASPageMetadata>;
 integration.addBuildEventHandler(
   "onPreBuild",
   async ({ utils: { run, cache } }) => {
-    console.log("Running redoc prebuild -- MONOREPO TEST!");
+    console.log("Running redoc prebuild");
     const hasRedoc = await cache.has("redoc");
 
     if (hasRedoc) {
