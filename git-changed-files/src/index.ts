784346a9f6d5137e2c260a4d510436d3cee2bcf3
biancalaube <bianca.laube@gmail.com>
2024-09-10 10:26:05 -0400

    new integration

---
+++ git-changed-files/src/index.ts
@@ -0,0 +1,15 @@
+import { NetlifyIntegration } from "@netlify/sdk";
+
+const integration = new NetlifyIntegration();
+
+integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
+  console.log("Checking if any files changed on git -----");
+  if (git.modifiedFiles.length !== 0) {
+    console.log('Modified files:', git.modifiedFiles)
+  }
+
+  status.show({
+    title: `Changed Files`,
+    summary: git.modifiedFiles,
+  });
+});
