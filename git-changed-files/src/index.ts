128cb4320328df6dd4693325ff2a5eb3867cc942
biancalaube <bianca.laube@gmail.com>
2024-09-11 10:17:20 -0400

    updates

--- git-changed-files/src/index.ts
+++ git-changed-files/src/index.ts
@@ -6,7 +6,7 @@ integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
   console.log("Checking if any files changed on git -----");
   if (git.modifiedFiles.length !== 0) {
     const arr =  git.modifiedFiles;
-    const pre = process.env.REPOSITORY_URL + '/blob/' + process.env.BRANCH;
+    const pre = process.env.REPOSITORY_URL + '/blob/' + process.env.HEAD + '/';
     const newArr = (pre + arr.join(';' + pre)).split(';');
     console.log('Modified files:', git.modifiedFiles, process.env.REPOSITORY_URL);
     console.log(newArr);
