e8508339153b8238d4964b0eac0e864622298e19
biancalaube <bianca.laube@gmail.com>
2024-09-11 09:48:44 -0400

    trying new array

--- git-changed-files/src/index.ts
+++ git-changed-files/src/index.ts
@@ -5,7 +5,10 @@ const integration = new NetlifyIntegration();
 integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
   console.log("Checking if any files changed on git -----");
   if (git.modifiedFiles.length !== 0) {
-    console.log('Modified files:', git.modifiedFiles, process.env.REPOSITORY_URL)
+    const arr =  git.modifiedFiles;
+    const newArr = (process.env.REPOSITORY_URL + arr.join(';' + process.env.REPOSITORY_URL)).split(';');
+    console.log('Modified files:', git.modifiedFiles, process.env.REPOSITORY_URL);
+    console.log(newArr);
   }
 
   status.show({
