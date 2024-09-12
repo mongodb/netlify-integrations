f17f38895c71a103ce1d2b2c3323e6ceb636361d
biancalaube <bianca.laube@gmail.com>
2024-09-12 13:23:51 -0400

    new function

--- git-changed-files/src/index.ts
+++ git-changed-files/src/index.ts
@@ -6,14 +6,7 @@ integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
   console.log("Checking if any files changed on git -----");
   console.log('Modified files:', git.modifiedFiles);
 
-  const markdownList = []
-  for (const modifiedFile of git.modifiedFiles) {
-    if (modifiedFile.includes('source') && (!modifiedFile.includes('/images') || !modifiedFile.includes('/includes') || !modifiedFile.includes('/examples'))) {
-      let shortform = modifiedFile.replace('source', '');
-      shortform = shortform.replace('.txt', '');
-      markdownList.push(`[${modifiedFile}](${process.env.DEPLOY_PRIME_URL + shortform})`);
-    }
-  }
+  const markdownList = createMarkdown(git.modifiedFiles);
 
   if (markdownList.length !== 0) {
     status.show({
@@ -22,5 +15,24 @@ integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
     });
   }
 });
+
+/**
+ * Function to convert git modifed files to links of the staged files. Needs to be in markdown
+ * so on the deploy summary the text appears as a link not just as text.
+ *
+ * @param modifiedFiles
+ */
+export function createMarkdown(modifiedFiles: String[]) {
+  const markdownList = []
+  for (const modifiedFile of modifiedFiles) {
+    if (modifiedFile.includes('source') && (!modifiedFile.includes('/images') || !modifiedFile.includes('/includes') || !modifiedFile.includes('/examples'))) {
+      let shortform = modifiedFile.replace('source', '');
+      shortform = shortform.replace('.txt', '');
+      markdownList.push(`[${modifiedFile}](${process.env.DEPLOY_PRIME_URL + shortform})`);
+    }
+  }
+
+  return markdownList;
+}
 
 export { integration };
