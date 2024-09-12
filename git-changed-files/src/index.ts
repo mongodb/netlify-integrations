f7f5b0dc39f6aa66563ebf5fd749e9d1dde214d8
biancalaube <bianca.laube@gmail.com>
2024-09-12 15:22:49 -0400

    adding test

--- git-changed-files/src/index.ts
+++ git-changed-files/src/index.ts
@@ -6,7 +6,12 @@ integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
   console.log("Checking if any files changed on git -----");
   console.log('Modified files:', git.modifiedFiles);
 
-  const markdownList = createMarkdown(git.modifiedFiles);
+  if (!process.env.DEPLOY_PRIME_URL) {
+    console.error('ERROR! process.env.DEPLOY_PRIME_URL is not defined.')
+    return;
+  }
+
+  const markdownList = createMarkdown(git.modifiedFiles, process.env.DEPLOY_PRIME_URL);
 
   if (markdownList.length !== 0) {
     status.show({
@@ -21,13 +26,27 @@ integration.addBuildEventHandler("onSuccess", ({utils: {status, git}}) => {
  * so on the deploy summary the text appears as a link not just as text.
  *
  * @param modifiedFiles
+ * @param netlifyURL
+ * @returns string[]
  */
-export function createMarkdown(modifiedFiles: readonly string[]) : string[] {
+export function createMarkdown(modifiedFiles: readonly string[], netlifyURL: string) : string[] {
+  const IGNORED_DIRS = new Set(['includes', 'images', 'examples']);
+
   const markdownList = []
   for (const modifiedFile of modifiedFiles) {
-    if (modifiedFile.includes('source') && (!modifiedFile.includes('/images') || !modifiedFile.includes('/includes') || !modifiedFile.includes('/examples'))) {
-      let shortform = modifiedFile.replace('source', '').replace('.txt', '');
-      markdownList.push(`[${modifiedFile}](${process.env.DEPLOY_PRIME_URL + shortform})`);
+
+    const modifiedFilePath = modifiedFile.split('/');
+    
+    // check if this is equal to 'source'
+    const isSourceDir = modifiedFilePath[0] === 'source'
+
+    // check if this is equal to either images, includes, or examples
+     const isNonIgnoredDir =  !IGNORED_DIRS.has(modifiedFilePath[1]);
+
+
+    if (isSourceDir && isNonIgnoredDir) {
+      const shortform = modifiedFile.replace('source', '').replace('.txt', '');
+      markdownList.push(`[${modifiedFile}](${netlifyURL + shortform})`);
     }
   }
 
