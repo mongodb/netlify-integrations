75cf55782cae6d4f5fb1cab4b756857868862fdc
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-09-17 14:15:26 -0400

    DOP-5018 more mock data, finished tests

--- search-manifest/tests/unit/getProperties.test.ts
+++ search-manifest/tests/unit/getProperties.test.ts
@@ -21,6 +21,7 @@ import { DatabaseDocument } from "../../src/uploadToAtlas/types";
 import { Manifest } from "../../src/generateManifest/manifest";
 import { getManifest } from "../utils/getManifest";
 import { uploadManifest } from "../../src/uploadToAtlas/uploadManifest";
+import { afterEach } from "node:test";
 
 const BRANCH_NAME_MASTER = "master";
 const BRANCH_NAME_BETA = "beta";
@@ -35,7 +36,6 @@ const repoNames = [
 ];
 
 beforeAll(async () => {
-  process.env.REPO_NAME = repoNames[0];
   db = await mockDb();
   await insert(db, "repos_branches", repos_branches);
   await insert(db, "docsets", docsets);
@@ -44,10 +44,10 @@ beforeAll(async () => {
 const removeDocuments = async () => {
   //delete all documents in repo
   await db.collection<DatabaseDocument>("repos_branches").deleteMany({});
-  const documentCount = await db
-    .collection<DatabaseDocument>("repos_branches")
-    .estimatedDocumentCount();
   await db.collection<DatabaseDocument>("documents").deleteMany({});
+  const documentCount = await db
+    .collection<DatabaseDocument>("documents")
+    .countDocuments();
 
   return documentCount;
 };
@@ -152,85 +152,145 @@ describe("Given a branchname, get the properties associated with it from repos_b
   });
 });
 
-describe("GetProperties behaves as expected for stale properties", async () => {
-  db = await mockDb();
-  const documentsColl = db.collection<DatabaseDocument>("documents");
+describe(
+  "GetProperties behaves as expected for stale properties",
+  async () => {
+    afterEach(async () => {
+      console.log(await removeDocuments());
+    });
 
-  beforeAll(async () => {
-    const manifest1 = await getManifest("mms-master");
-    await uploadManifest(manifest1, "mms-docs-master");
-    const manifest2 = await getManifest("mms-v1.3");
-    await uploadManifest(manifest1, "mms-docs-v1.3");
-
-    db = await mockDb();
-    let documentCount = await db
-      .collection<DatabaseDocument>("documents")
-      .estimatedDocumentCount();
-    expect(documentCount).toEqual(1296);
-  });
-
-  afterAll(async () => {
-    await removeDocuments();
-  });
-
-  //TODO
-  test("getting properties for an inactive branch with no existing documents executes correctly and does not change db document count", async () => {
-    db = await mockDb();
-    let documentCount;
-    documentCount = await db
-      .collection<DatabaseDocument>("documents")
-      .estimatedDocumentCount();
-    //getProperties for beta
-    process.env.repo_name = "docs-compass";
-    await expect(getProperties(BRANCH_NAME_BETA)).rejects.toThrow();
-    console.log(documentCount);
-    expect(
-      await db
+    test("getting properties for an inactive branch with no existing documents executes correctly and does not change db document count", async () => {
+      //populate db with manifests
+      db = await mockDb();
+      const manifest1 = await getManifest("mms-master");
+      console.log("manifest 1 number of docs", manifest1.documents.length);
+      const status = await uploadManifest(manifest1, "mms-docs-stable");
+      //reopen connection to db
+      db = await mockDb();
+      //check number of documents initially in db
+      const documentCount = await db
         .collection<DatabaseDocument>("documents")
-        .estimatedDocumentCount()
-    ).toEqual(documentCount);
-  });
+        .countDocuments();
 
-  test("non prod-deployable repo throws and doesn't return properties", async () => {
-    process.env.REPO_NAME = repoNames[3];
-    //TODO: expect deleteproperties to be called
-    //throws
-    await expect(getProperties("v5.0")).rejects.toThrow(
-      `Search manifest should not be generated for repo ${process.env.REPO_NAME}. Removing all associated manifests`
-    );
-  });
+      //getProperties for beta doens't change number of documents in collection
+      process.env.repo_name = "docs-compass";
+      await expect(getProperties(BRANCH_NAME_BETA)).rejects.toThrow();
+      expect(
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      ).toEqual(documentCount);
+    });
 
-  test(`no properties are retrieved for branch on repo ${repoNames[2]} without a "search" field. `, async () => {
-    //define expected properties object for master branch of docs-app-services repo
-    process.env.REPO_NAME = repoNames[2];
-    await expect(getProperties(BRANCH_NAME_MASTER)).rejects.toThrow();
-  });
+    test("non prod-deployable repo throws and doesn't return properties", async () => {
+      process.env.REPO_NAME = repoNames[3];
+      await expect(getProperties("v5.0")).rejects.toThrow(
+        `Search manifest should not be generated for repo ${process.env.REPO_NAME}. Removing all associated manifests`
+      );
+    });
 
-  test("repo with no search categoryTitle removes all old documents with search properties beginning with that project name", async () => {
-    //add documents for project from two diff branches to search DB
-    db = await mockDb();
-    let documentCount;
+    test(`no properties are retrieved for branch on repo ${repoNames[2]} without a "search" field. `, async () => {
+      process.env.REPO_NAME = repoNames[2];
+      await expect(getProperties(BRANCH_NAME_MASTER)).rejects.toThrow();
+    });
 
-    //trying to get properties for repo removes those older documents
-    process.env.repo_name = "mms-docs";
-    await expect(getProperties(BRANCH_NAME_MASTER)).rejects.toThrow();
-    //throws
-    //no return type
+    test("repo with no search categoryTitle removes all old documents with search properties beginning with that project name", async () => {
+      db = await mockDb();
+      console.log(
+        "ORIGINAL document count",
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      );
+      //add documents for project from two diff branches to search DB
+      const manifest1 = await getManifest("mms-master");
+      console.log("manifest 1 number of docs", manifest1.documents.length);
 
-    db = await mockDb();
-    const doc = await db.collection<DatabaseDocument>("documents").findOne();
-    //TODO
-    documentCount = await db
-      .collection<DatabaseDocument>("documents")
-      .estimatedDocumentCount();
-    expect(documentCount).toEqual(0);
-  });
+      const status = await uploadManifest(manifest1, "mms-docs-stable");
+      db = await mockDb();
 
-  //TODO:
-  test("getting properties for an inactive branch removes all old documents with that exact project-version searchProperty", () => {
-    //add documents for project from two diff branches to DB-- docs-compass master and beta
-    //trying to get properties for repo removes only the older documents from that specific branch, beta
-    //throws
-    //no return type
-  });
-});
+      console.log(
+        "document count after first upload",
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      );
+      console.log("STATUS UPSERTED:", status.upserted);
+      const manifest2 = await getManifest("mms-v1.3");
+      console.log("manifest 2 number of docs", manifest2.documents.length);
+      const status2 = await uploadManifest(manifest2, "mms-docs-v1.3");
+      console.log("STATUS2 UPSERTED:", status.upserted);
+
+      db = await mockDb();
+      console.log(
+        "document count after second upload",
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      );
+      console.log("STATUS2 UPSERTED:", status2.upserted);
+
+      let documentCount;
+      let documentCount2;
+      //trying to get properties for repo removes those older documents
+      process.env.REPO_NAME = "mms-docs";
+      documentCount = await db
+        .collection<DatabaseDocument>("documents")
+        .countDocuments();
+      await expect(getProperties(BRANCH_NAME_MASTER)).rejects.toThrow();
+      //throws
+      //no return type
+
+      db = await mockDb();
+      documentCount2 = await db
+        .collection<DatabaseDocument>("documents")
+        .countDocuments();
+      console.log(documentCount, documentCount2);
+      expect(documentCount2).toEqual(
+        documentCount - manifest1.documents.length - manifest2.documents.length
+      );
+    });
+
+    test("getting properties for an inactive branch removes all old documents with that exact project-version searchProperty", async () => {
+      //add documents for project from two diff branches to DB-- docs-compass master and beta
+      db = await mockDb();
+      console.log(
+        "ORIGINAL document count",
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      );
+      //add documents for project from two diff branches to search DB
+      const manifest1 = await getManifest("compass-master");
+      console.log("manifest 1 number of docs", manifest1.documents.length);
+
+      const status = await uploadManifest(manifest1, "compass-current");
+      db = await mockDb();
+
+      console.log(
+        "document count after first upload",
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      );
+      console.log("STATUS UPSERTED:", status.upserted);
+      const manifest2 = await getManifest("compass-beta");
+      console.log("manifest 2 number of docs", manifest2.documents.length);
+      const status2 = await uploadManifest(manifest2, "compass-upcoming");
+      db = await mockDb();
+      console.log(
+        "document count after second upload",
+        await db.collection<DatabaseDocument>("documents").countDocuments()
+      );
+      console.log("STATUS2 UPSERTED:", status2.upserted);
+
+      //trying to get properties for repo removes only the older documents from that specific branch, beta
+      let documentCount;
+      let documentCount2;
+      //trying to get properties for repo removes those older documents
+      process.env.REPO_NAME = "docs-compass";
+      documentCount = await db
+        .collection<DatabaseDocument>("documents")
+        .countDocuments();
+      await expect(getProperties(BRANCH_NAME_BETA)).rejects.toThrow();
+
+      db = await mockDb();
+      documentCount2 = await db
+        .collection<DatabaseDocument>("documents")
+        .countDocuments();
+      console.log(documentCount, documentCount2);
+      expect(documentCount2).toEqual(
+        documentCount - manifest2.documents.length
+      );
+    });
+  },
+  { timeout: 10000 }
+);
