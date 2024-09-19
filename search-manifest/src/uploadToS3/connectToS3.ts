472d3a52a1940c2cf27764c077e81fcd37055383
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-09-19 14:47:59 -0400

    DOP-4830 try pushing up without credentials

--- search-manifest/src/uploadToS3/connectToS3.ts
+++ search-manifest/src/uploadToS3/connectToS3.ts
@@ -2,9 +2,9 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const connectToS3 = () => {
   const client = new S3Client({
-    credentials: {
-      accessKeyId: "",
-      secretAccessKey: "",
-    },
+    // credentials: {
+    //   accessKeyId: "",
+    //   secretAccessKey: "",
+    // },
   });
 };
