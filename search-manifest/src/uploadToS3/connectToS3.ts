8d1ef1a278acdae5aa23ecf8cf7f9a84ecf1818e
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-09-20 09:59:06 -0400

    DOP-4830 add try catch to connection

--- search-manifest/src/uploadToS3/connectToS3.ts
+++ search-manifest/src/uploadToS3/connectToS3.ts
@@ -1,12 +1,17 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const connectToS3 = (): S3Client => {
-  const client = new S3Client({
-    credentials: {
-      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID ?? "",
-      secretAccessKey: process.env.AWS_S3_SECRET_ACCESSS_KEY ?? "",
-    },
-    region: "us-east-2",
-  });
-  return client;
+  if (
+    process.env.AWS_S3_ACCESS_KEY_ID &&
+    process.env.AWS_S3_SECRET_ACCESSS_KEY
+  ) {
+    const client = new S3Client({
+      credentials: {
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_S3_SECRET_ACCESSS_KEY,
+      },
+      region: "us-east-2",
+    });
+    return client;
+  } else throw new Error("credentials not found");
 };
