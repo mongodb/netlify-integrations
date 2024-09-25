9b21943a14b875ef1a5d6cf39c4fe5c476f0d6a7
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-09-25 16:00:49 -0400

    DOP-5023 run mut

--- snooty-cache/src/index.ts
+++ snooty-cache/src/index.ts
@@ -60,6 +60,8 @@ integration.addBuildEventHandler("onSuccess", async ({ utils: { run } }) => {
     "curl -L -o mut.zip https://github.com/mongodb/mut/releases/download/v0.11.4/mut-v0.11.4-linux_x86_64.zip"
   );
   await run.command("unzip -d . mut.zip");
+  run.command("ls -a");
+
   await run.command("mut-redirects snooty/config/redirects -o .htaccess");
   // run.command("ls -a");
 });
