bfe90d1a110b7ca4ac09d7a079b70eb8ca38a776
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-09-27 12:41:19 -0400

    DOP-5023 add try catch block

--- snooty-cache/src/index.ts
+++ snooty-cache/src/index.ts
@@ -1,109 +1,110 @@
 // Documentation: https://sdk.netlify.com
 
-import { NetlifyIntegration } from '@netlify/sdk';
+import { NetlifyIntegration } from "@netlify/sdk";
 
+import { readdir } from "fs";
 
-import { readdir } from 'fs';
-
-import { promisify } from 'util';
-import { checkForNewSnootyVersion } from './snooty-frontend-version-check';
-
+import { promisify } from "util";
+import { checkForNewSnootyVersion } from "./snooty-frontend-version-check";
 
 const readdirAsync = promisify(readdir);
 
 const MUT_VERSION = "0.11.4";
 
 const getCacheFilePaths = (filesPaths: string[]): string[] =>
-	filesPaths.filter((filePath) => filePath.endsWith('.cache.gz'));
+  filesPaths.filter((filePath) => filePath.endsWith(".cache.gz"));
 
 const integration = new NetlifyIntegration();
 
 integration.addBuildEventHandler(
-	'onPreBuild',
-	async ({ utils: { cache, run } }) => {
-		const files: string[] = await cache.list();
+  "onPreBuild",
+  async ({ utils: { cache, run } }) => {
+    const files: string[] = await cache.list();
 
-		const cacheFiles = getCacheFilePaths(files);
+    const cacheFiles = getCacheFilePaths(files);
 
-		if (!cacheFiles.length) {
-			console.log('No snooty cache files found');
+    if (!cacheFiles.length) {
+      console.log("No snooty cache files found");
 
-			return;
-		}
-		// Don't want to restore duplicates, only restore snooty cache files
-		console.log('restoring snooty cache files');
+      return;
+    }
+    // Don't want to restore duplicates, only restore snooty cache files
+    console.log("restoring snooty cache files");
 
-		await Promise.all(cacheFiles.map((cacheFile) => cache.restore(cacheFile)));
+    await Promise.all(cacheFiles.map((cacheFile) => cache.restore(cacheFile)));
 
-		await checkForNewSnootyVersion(run);
-	},
+    await checkForNewSnootyVersion(run);
+  }
 );
 
 integration.addBuildEventHandler(
-	'onSuccess',
-	async ({ utils: { run, cache } }) => {
-		console.log('Creating cache files...');
-		await run.command('./snooty-parser/snooty/snooty create-cache .');
-		console.log('Cache files created');
-		const filesPaths = await readdirAsync(process.cwd());
+  "onSuccess",
+  async ({ utils: { run, cache } }) => {
+    console.log("Creating cache files...");
+    await run.command("./snooty-parser/snooty/snooty create-cache .");
+    console.log("Cache files created");
+    const filesPaths = await readdirAsync(process.cwd());
 
-		const cacheFiles = getCacheFilePaths(filesPaths);
+    const cacheFiles = getCacheFilePaths(filesPaths);
 
-		await Promise.all(
-			cacheFiles.map(async (filePath) => {
-				console.log(`Adding cache file: ${filePath}`);
-				await cache.save(filePath);
-			}),
-		);
-	},
+    await Promise.all(
+      cacheFiles.map(async (filePath) => {
+        console.log(`Adding cache file: ${filePath}`);
+        await cache.save(filePath);
+      })
+    );
+  }
 );
+try {
+  integration.addBuildEventHandler("onSuccess", async ({ utils: { run } }) => {
+    console.log("Downloading Mut...");
+    await run.command(
+      `curl -L -o mut.zip https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`
+    );
+    await run.command("unzip -d . mut.zip");
 
-integration.addBuildEventHandler("onSuccess", async ({ utils: { run } }) => {
-  console.log("Downloading Mut...");
-  await run.command(
-    `curl -L -o mut.zip https://github.com/mongodb/mut/releases/download/v${MUT_VERSION}/mut-v${MUT_VERSION}-linux_x86_64.zip`
+    console.log("Running mut-redirects...");
+    await run.command(
+      `${process.cwd()}/mut/mut-redirects config/redirects -o snooty/public/.htaccess`
+    );
+  });
+} catch (e) {
+  throw new Error(`Error while running mut redirects: ${e}`);
+} finally {
+  integration.addBuildEventHandler(
+    "onEnd",
+    async ({ utils: { run, status } }) => {
+      console.log("Creating cache files...");
+      const { all, stderr, stdout } = await run.command(
+        "./snooty-parser/snooty/snooty create-cache .",
+        { all: true }
+      );
+
+      const logs = all ?? stdout + stderr;
+
+      const logsSplit =
+        logs
+          .split("\n")
+          .filter(
+            (row) =>
+              !row.includes("INFO:snooty.gizaparser.domain") &&
+              !row.includes("INFO:snooty.parser:cache")
+          ) || [];
+
+      let errorCount = 0;
+      let warningCount = 0;
+
+      logsSplit.forEach((row) => {
+        if (row.includes("ERROR")) errorCount += 1;
+        if (row.includes("WARNING")) warningCount += 1;
+      });
+
+      status.show({
+        title: `Snooty Parser Logs - Errors: ${errorCount} | Warnings: ${warningCount}`,
+        summary: logsSplit.join("\n"),
+      });
+    }
   );
-  await run.command("unzip -d . mut.zip");
-
-  console.log("Running mut-redirects...");
-  await run.command(
-    `${process.cwd()}/mut/mut-redirects config/redirects -o snooty/public/.htaccess`
-  );
-});
-
-integration.addBuildEventHandler(
-	'onEnd',
-	async ({ utils: { run, status } }) => {
-		console.log('Creating cache files...');
-		const { all, stderr, stdout } = await run.command(
-			'./snooty-parser/snooty/snooty create-cache .',
-			{ all: true },
-		);
-
-		const logs = all ?? stdout + stderr;
-
-		const logsSplit =
-			logs
-				.split('\n')
-				.filter(
-					(row) =>
-						!row.includes('INFO:snooty.gizaparser.domain') &&
-						!row.includes('INFO:snooty.parser:cache'),
-				) || [];
-
-		let errorCount = 0;
-		let warningCount = 0;
-
-		logsSplit.forEach((row) => {
-			if (row.includes('ERROR')) errorCount += 1;
-			if (row.includes('WARNING')) warningCount += 1;
-		});
-
-		status.show({
-			title: `Snooty Parser Logs - Errors: ${errorCount} | Warnings: ${warningCount}`,
-			summary: logsSplit.join('\n'),
-		});
-	},
-);
+}
 
 export { integration };
