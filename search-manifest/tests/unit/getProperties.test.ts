d2bb40b35a1af1ba5117646873d4ba385d488415
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-09-27 10:37:43 -0400

    DOP-4830: Smartling can ingest search manifests from Netlify (#7)
    * DOP-5018 add logic for deleting stale properties, documents
    
    * DOP-5018 tests for deleteStale WIP
    
    * DOP-5018 improvements made while writing tests
    
    * DOP-5018 more mock data, finished tests
    
    * DOP-5018 add last test
    
    * DOP-5018 cleaning up
    
    * DOP-4830 initial logic for uploading manifests to s3
    
    * DOP-4830 try pushing up without credentials
    
    * DOP-4830 add credentials
    
    * DOP-4830 add region
    
    * DOP-4830 add try catch to connection
    
    * DOP-4830 put credentials in consts
    
    * DOP-4830 fix typo
    
    * DOP-4830 change comment
    
    * DOP-4830 refactor and add s3 upload test
    
    * DOP-4830 cleaning
    
    * DOP-4830 cleaning
    
    * DOP-4830 cleaning
    
    * DOP-4830 cleaning tests
    
    * DOP-4830 addressing comments
    
    * DOP-5023 addressing nits

--- search-manifest/tests/unit/getProperties.test.ts
+++ search-manifest/tests/unit/getProperties.test.ts
@@ -1,27 +1,27 @@
 import {
-	describe,
-	beforeEach,
-	expect,
-	test,
-	vi,
-	beforeAll,
-	afterAll,
-} from 'vitest';
+  describe,
+  beforeEach,
+  expect,
+  test,
+  vi,
+  beforeAll,
+  afterAll,
+} from "vitest";
 import getProperties, {
   getBranch,
 } from "../../src/uploadToAtlas/getProperties";
 import {
-	mockDb,
-	teardownMockDbClient,
-	insert,
-	removeDocuments,
-} from '../utils/mockDB';
+  mockDb,
+  teardownMockDbClient,
+  insert,
+  removeDocuments,
+} from "../utils/mockDB";
 // simulate the repos_branches collection in an object
-import repos_branches from '../resources/mockCollections/repos-branches.json';
+import repos_branches from "../resources/mockCollections/repos-branches.json";
 //simulate the docsests collection in an object
 import docsets from "../resources/mockCollections/docsets.json";
 import * as mongodb from "mongodb";
-import { BranchEntry, DatabaseDocument } from "../../src/uploadToAtlas/types";
+import { BranchEntry, DatabaseDocument } from "../../src/types";
 import { Manifest } from "../../src/generateManifest/manifest";
 import { getManifest } from "../utils/getManifest";
 import { uploadManifest } from "../../src/uploadToAtlas/uploadManifest";
@@ -39,7 +39,6 @@ const DOCS_MONGODB_INTERNAL_NAME = "docs-mongodb-internal";
 
 beforeAll(async () => {
   db = await mockDb();
-
   await insert(db, "repos_branches", repos_branches);
   await insert(db, "docsets", docsets);
 });
@@ -60,9 +59,9 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
-	//teardown db instance
-	await removeDocuments('repos_branches');
-	await teardownMockDbClient();
+  //teardown db instance
+  await removeDocuments("repos_branches");
+  await teardownMockDbClient();
 });
 
 describe("given an array of branches and a branch name, the corrct output is returned", () => {
@@ -99,26 +98,28 @@ describe("given an array of branches and a branch name, the corrct output is ret
 });
 
 //two tests for a repo with multiple branches, one test for a repo with only one branch
-describe('Given a branchname, get the properties associated with it from repos_branches', () => {
-	//mock repo name
-	test(`correct properties are retrieved for branch ${BRANCH_NAME_MASTER} of repoName ${DOCS_COMPASS_NAME}`, async () => {
-		//define expected properties object for master branch of Compass repo
-		process.env.REPO_NAME = DOCS_COMPASS_NAME;
-		const compassMasterProperties = {
-			searchProperty: 'compass-current',
-			url: 'http://mongodb.com/docs/compass/',
-			includeInGlobalSearch: true,
-		};
-		expect(await getProperties(BRANCH_NAME_MASTER)).toEqual(
-			compassMasterProperties,
-		);
-	});
+describe("Given a branchname, get the properties associated with it from repos_branches", () => {
+  //mock repo name
+  test(`correct properties are retrieved for branch ${BRANCH_NAME_MASTER} of repoName ${DOCS_COMPASS_NAME}`, async () => {
+    //define expected properties object for master branch of Compass repo
+    process.env.REPO_NAME = DOCS_COMPASS_NAME;
+    const compassMasterProperties = {
+      searchProperty: "compass-current",
+      projectName: "compass",
+      url: "http://mongodb.com/docs/compass/",
+      includeInGlobalSearch: true,
+    };
+    expect(await getProperties(BRANCH_NAME_MASTER)).toEqual(
+      compassMasterProperties
+    );
+  });
 
   test(`correct properties are retrieved for branch ${BRANCH_NAME_MASTER} of repoName ${DOCS_CLOUD_NAME}`, async () => {
     //define expected properties object for master branch of cloud-docs repo
     process.env.REPO_NAME = DOCS_CLOUD_NAME;
     const cloudDocsMasterProperties = {
       searchProperty: "atlas-master",
+      projectName: "cloud-docs",
       url: "http://mongodb.com/docs/atlas/",
       includeInGlobalSearch: true,
     };
@@ -151,7 +152,6 @@ describe(
       //getProperties for beta doens't change number of documents in collection
       process.env.repo_name = "docs-compass";
       await expect(getProperties(BRANCH_NAME_BETA)).rejects.toThrow();
-      await mockDb();
       expect(
         await db.collection<DatabaseDocument>("documents").countDocuments()
       ).toEqual(documentCount);
@@ -194,6 +194,7 @@ describe(
 
       await mockDb();
       const documentCount2 = await db
+
         .collection<DatabaseDocument>("documents")
         .countDocuments();
       expect(documentCount2).toEqual(
