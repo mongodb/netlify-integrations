c24bfe135bd0821e4ceb08618a1729181cc53cc1
branberry <brandon.ly@mongodb.com>
2024-10-09 11:23:46 -0500

    Add back in addFunctions

--- slack/src/index.ts
+++ slack/src/index.ts
@@ -3,13 +3,13 @@ import { NetlifyExtension } from '@netlify/sdk';
 
 const extension = new NetlifyExtension();
 
-// extension.addFunctions('./src/functions', {
-// 	prefix: 'test',
-// 	shouldInjectFunction: ({ name }) => {
-// 		console.log('name!!! ', name);
-// 		// If the function is not enabled, return early
-// 		return true;
-// 	},
-// });
+extension.addFunctions('./src/functions', {
+	prefix: 'test',
+	shouldInjectFunction: ({ name }) => {
+		console.log('name!!! ', name);
+		// If the function is not enabled, return early
+		return true;
+	},
+});
 
 export { extension };
