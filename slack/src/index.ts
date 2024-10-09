b06720c25f2765cc2a274c6de7da6b1a39125ddc
branberry <brandon.ly@mongodb.com>
2024-10-09 10:46:06 -0500

    refactor

--- slack/src/index.ts
+++ slack/src/index.ts
@@ -1,18 +1,14 @@
 // Documentation: https://sdk.netlify.com
-import { NetlifyExtension } from "@netlify/sdk";
+import { NetlifyExtension } from '@netlify/sdk';
 
 const extension = new NetlifyExtension();
 
-extension.addFunctions("./src/functions", {
-  prefix: "my_unique_prefix",
-  shouldInjectFunction: () => {
-    // If the function is not enabled, return early
-    if (!process.env["SLACK_ENABLED"]) {
-      return;
-    }
-    return true;
-  },
+extension.addFunctions('./src/functions', {
+	prefix: 'my_unique_prefix',
+	shouldInjectFunction: () => {
+		// If the function is not enabled, return early
+		return !!process.env.SLACK_ENABLED;
+	},
 });
 
 export { extension };
-
