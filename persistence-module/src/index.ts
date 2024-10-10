ef115230e10e244ec536ce6f4fa8171efaa42aed
biancalaube <bianca.laube@gmail.com>
2024-10-10 15:59:16 -0400

    formatter

--- persistence-module/src/index.ts
+++ persistence-module/src/index.ts
@@ -6,13 +6,13 @@ const extension = new NetlifyExtension();
 const ZIP_PATH = `${process.cwd()}/bundle/documents`;
 
 extension.addBuildEventHandler(
-  'onPreBuild',
-  async ({ utils: { cache, run } }) => {
-    try {
-      await downloadPersistenceModule(run);
-    } catch (e) {
-      console.error('Unable to run the persistence module', e);
-    }
-  },
+	'onPreBuild',
+	async ({ utils: { cache, run } }) => {
+		try {
+			await downloadPersistenceModule(run);
+		} catch (e) {
+			console.error('Unable to run the persistence module', e);
+		}
+	},
 );
 export { extension };
