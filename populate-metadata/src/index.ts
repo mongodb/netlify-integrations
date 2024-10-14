bceaf1929365a4666a44fc4fed2afa375ebc2779
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-10-14 17:29:17 -0400

    DOP-5071 extension creation

---
+++ populate-metadata/src/index.ts
@@ -0,0 +1,15 @@
+// Documentation: https://sdk.netlify.com
+import { NetlifyExtension } from "@netlify/sdk";
+
+const extension = new NetlifyExtension();
+
+extension.addBuildEventHandler("onPreBuild", () => {
+  // If the build event handler is not enabled, return early
+  if (!process.env["POPULATE_METADATA_ENABLED"]) {
+    return;
+  }
+  console.log("Hello there.");
+});
+  
+export { extension };
+
