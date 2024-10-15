4cb97caeb041179f0814ce5f88df8603df22945e
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-10-15 12:21:44 -0400

    DOP-5071 log config

--- populate-metadata/src/index.ts
+++ populate-metadata/src/index.ts
@@ -10,7 +10,7 @@ extension.addBuildEventHandler('onPreBuild', async ({ netlifyConfig }) => {
     return;
   }
   console.log('Hello there.');
-  console.log(netlifyConfig?);
+  console.log(netlifyConfig);
 });
 
 export { extension };
