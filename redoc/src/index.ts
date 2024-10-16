607400be01afcf3e4f9bb9eee08f4b698d399644
biancalaube <bianca.laube@gmail.com>
2024-10-16 11:15:22 -0400

    formatting

--- redoc/src/index.ts
+++ redoc/src/index.ts
@@ -16,71 +16,67 @@ export interface OASPageMetadata {
 
 export type OASPagesMetadata = Record<string, OASPageMetadata>;
 
-
-	console.log("IN REDOC: before check");
-	console.log("IN REDOC: before check");
-
-	// handle installing redoc cli if it's not already installed
-	extension.addBuildEventHandler(
-		'onPreBuild',
-		async ({ utils: { run, cache } }) => {
-			console.log('Running redoc prebuild');
-			if (!process.env.EXTENSION_ENABLED) return;
-			const hasRedoc = await cache.has('redoc');
-
-			if (hasRedoc) {
-				console.log('Restoring redoc from cache');
-				cache.restore('redoc');
-				return;
-			}
-
-			await run.command(
-				`git clone -b @dop/redoc-cli@${REDOC_CLI_VERSION} --depth 1 https://github.com/mongodb-forks/redoc.git redoc`,
-			);
-
-			await run.command('npm ci --prefix cli/ --omit=dev', {
-				cwd: `${process.cwd()}/redoc`,
-			});
-
-			await cache.save('redoc');
-		},
-	);
-
-	// handle building the redoc pages
-	extension.addBuildEventHandler('onPostBuild', async ({ utils: { run } }) => {
+// handle installing redoc cli if it's not already installed
+extension.addBuildEventHandler(
+	'onPreBuild',
+	async ({ utils: { run, cache } }) => {
+		console.log('Running redoc prebuild');
 		if (!process.env.EXTENSION_ENABLED) return;
-		console.log('=========== Redoc Extension Begin ================');
-		await run.command('unzip -o bundle.zip -d bundle');
+		const hasRedoc = await cache.has('redoc');
 
-		const siteBson = await readFileAsync(`${BUNDLE_PATH}/site.bson`);
-
-		const buildMetadata = deserialize(siteBson);
-		const siteTitle: string = buildMetadata.title;
-		const openapiPages: OASPagesMetadata | undefined =
-			buildMetadata.openapi_pages;
-
-		if (!openapiPages) {
-			console.log('No OpenAPI pages found');
+		if (hasRedoc) {
+			console.log('Restoring redoc from cache');
+			cache.restore('redoc');
 			return;
 		}
 
-		const openapiPagesEntries = Object.entries(openapiPages);
-		const siteUrl = process.env.DEPLOY_PRIME_URL || '';
+		await run.command(
+			`git clone -b @dop/redoc-cli@${REDOC_CLI_VERSION} --depth 1 https://github.com/mongodb-forks/redoc.git redoc`,
+		);
 
-		await buildOpenAPIPages(openapiPagesEntries, { siteTitle, siteUrl }, run);
+		await run.command('npm ci --prefix cli/ --omit=dev', {
+			cwd: `${process.cwd()}/redoc`,
+		});
 
-		console.log('=========== Redoc Extension End ================');
-	});
+		await cache.save('redoc');
+	},
+);
 
-	// cache redoc
-	extension.addBuildEventHandler('onSuccess', async ({ utils: { cache } }) => {
-		if (!process.env.EXTENSION_ENABLED) return;
-		const hasRedoc = await cache.has('redoc');
-		if (!hasRedoc) {
-			console.log('saving redoc to cache');
-			await cache.save('redoc');
-		}
-	});
+// handle building the redoc pages
+extension.addBuildEventHandler('onPostBuild', async ({ utils: { run } }) => {
+	if (!process.env.EXTENSION_ENABLED) return;
+	console.log('=========== Redoc Extension Begin ================');
+	await run.command('unzip -o bundle.zip -d bundle');
+
+	const siteBson = await readFileAsync(`${BUNDLE_PATH}/site.bson`);
+
+	const buildMetadata = deserialize(siteBson);
+	const siteTitle: string = buildMetadata.title;
+	const openapiPages: OASPagesMetadata | undefined =
+		buildMetadata.openapi_pages;
+
+	if (!openapiPages) {
+		console.log('No OpenAPI pages found');
+		return;
+	}
+
+	const openapiPagesEntries = Object.entries(openapiPages);
+	const siteUrl = process.env.DEPLOY_PRIME_URL || '';
+
+	await buildOpenAPIPages(openapiPagesEntries, { siteTitle, siteUrl }, run);
+
+	console.log('=========== Redoc Extension End ================');
+});
+
+// cache redoc
+extension.addBuildEventHandler('onSuccess', async ({ utils: { cache } }) => {
+	if (!process.env.EXTENSION_ENABLED) return;
+	const hasRedoc = await cache.has('redoc');
+	if (!hasRedoc) {
+		console.log('saving redoc to cache');
+		await cache.save('redoc');
+	}
+});
 
 
 
