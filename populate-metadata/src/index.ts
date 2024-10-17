257bc832abbaeab933756923e580aab8a83533ce
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-10-17 14:22:42 -0700

    DOP-5071 move function into separate file

--- populate-metadata/src/index.ts
+++ populate-metadata/src/index.ts
@@ -2,54 +2,17 @@
 import { NetlifyExtension } from '@netlify/sdk';
 import { getProperties } from './getProperties';
 import { Extension } from './initialization';
-import { updateConfig } from './updateConfig';
+import { updateConfig, updateConfigReal } from './updateConfig';
 
 const extension = new Extension(process.env.POPULATE_METADATA_ENABLED);
 if (extension.extensionEnabled) {
   extension.addBuildEventHandler('onPreBuild', async ({ netlifyConfig }) =>
     updateConfig(),
   );
-
-  extension.addBuildEventHandler('onPreBuild', async ({ netlifyConfig }) => {
-    // If the build event handler is not enabled on given site, return early
-
-    if (!process.env.POPULATE_METADATA_ENABLED) return;
-    const environmentConfig = netlifyConfig.build.environment;
-
-    console.log(environmentConfig);
-
-    //check if build was triggered by a webhook (If so, it was a prod deploy);
-    const isProdDeploy = !!(
-      netlifyConfig.build.environment?.INCOMING_HOOK_URL &&
-      netlifyConfig.build.environment?.INCOMING_HOOK_TITLE &&
-      netlifyConfig.build.environment?.INCOMING_HOOK_BODY
-    );
-    netlifyConfig.build.environment.PRODUCTION = isProdDeploy;
-
-    const branchName =
-      process.env.BRANCH_NAME ?? netlifyConfig.build?.environment.BRANCH;
-    const repoName =
-      process.env.REPO_NAME ?? netlifyConfig.build?.environment.SITE_NAME;
-
-    //set environment to dotcomprd or prd if it is a writer build, only Mongodb-Snooty site name pre-configured
-    process.env.ENV ??= isProdDeploy ? 'dotcomprd' : 'prd';
-
-    const { repo, docsetEntry } = await getProperties({
-      branchName: branchName,
-      repoName: repoName,
-    });
-    const { branches: branch, ...repoEntry } = repo;
-    netlifyConfig.build.environment.REPO = repoEntry;
-    netlifyConfig.build.environment.DOCSET = docsetEntry;
-    netlifyConfig.build.environment.BRANCH = branch;
-
-    console.log(
-      netlifyConfig.build.environment.PRODUCTION,
-      netlifyConfig.build.environment.REPO,
-      netlifyConfig.build.environment.DOCSET,
-      netlifyConfig.build.environment.BRANCH,
-    );
-  });
 }
+
+extension.addBuildEventHandler('onPreBuild', async ({ netlifyConfig }) =>
+  updateConfigReal(netlifyConfig),
+);
 
 export { extension };
--- populate-metadata/src/updateConfig.ts
+++ populate-metadata/src/updateConfig.ts
@@ -9,3 +9,44 @@ export const updateConfig = async () => {
   }
   console.log('In update config function');
 };
+
+export const updateConfigReal = async (netlifyConfig: any) => {
+  {
+    // If the build event handler is not enabled on given site, return early
+    const environmentConfig = netlifyConfig.build.environment;
+
+    console.log(environmentConfig);
+
+    //check if build was triggered by a webhook (If so, it was a prod deploy);
+    const isProdDeploy = !!(
+      netlifyConfig.build.environment?.INCOMING_HOOK_URL &&
+      netlifyConfig.build.environment?.INCOMING_HOOK_TITLE &&
+      netlifyConfig.build.environment?.INCOMING_HOOK_BODY
+    );
+    netlifyConfig.build.environment.PRODUCTION = isProdDeploy;
+
+    const branchName =
+      process.env.BRANCH_NAME ?? netlifyConfig.build?.environment.BRANCH;
+    const repoName =
+      process.env.REPO_NAME ?? netlifyConfig.build?.environment.SITE_NAME;
+
+    //set environment to dotcomprd or prd if it is a writer build, only Mongodb-Snooty site name pre-configured
+    process.env.ENV ??= isProdDeploy ? 'dotcomprd' : 'prd';
+
+    const { repo, docsetEntry } = await getProperties({
+      branchName: branchName,
+      repoName: repoName,
+    });
+    const { branches: branch, ...repoEntry } = repo;
+    netlifyConfig.build.environment.REPO = repoEntry;
+    netlifyConfig.build.environment.DOCSET = docsetEntry;
+    netlifyConfig.build.environment.BRANCH = branch;
+
+    console.log(
+      netlifyConfig.build.environment.PRODUCTION,
+      netlifyConfig.build.environment.REPO,
+      netlifyConfig.build.environment.DOCSET,
+      netlifyConfig.build.environment.BRANCH,
+    );
+  }
+};
